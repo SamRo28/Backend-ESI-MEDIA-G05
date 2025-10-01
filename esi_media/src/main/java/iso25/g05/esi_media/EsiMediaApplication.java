277e1c2ac48191daf37901f7dd9eee1817509108
SamRo28 <Samuel.Rodero@alu.uclm.es>
2025-10-01 19:33:34 +0200

    Inicializacion del proyecto Maven

---
+++ esi_media/src/main/java/iso25/g05/esi_media/EsiMediaApplication.java
@@ -0,0 +1,13 @@
+package iso25.g05.esi_media;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsiMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsiMediaApplication.class, args);
+	}
+
+}
---
+++ esi_media/src/test/java/iso25/g05/esi_media/EsiMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package iso25.g05.esi_media;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsiMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
